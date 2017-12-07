c55cda784484cc69665676c9cf80f802aa6d501b
adri <adrien.gonzalez@gmail.com>
2017-12-07 01:08:13 +0100

    New KeyGengit checkout -b Adri

--- src/com/adribast/clavarnak/StrongAES.java
+++ src/com/adribast/clavarnak/StrongAES.java
@@ -1,4 +1,5 @@
 package com.adribast.clavarnak;
+import java.io.UnsupportedEncodingException;
 import java.security.*;
 
 import java.security.InvalidKeyException;
@@ -9,78 +10,83 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-    public class StrongAES
-    {
+    public class StrongAES {
 
-                private String maChaine ="Jecrisseizecarac";
-                private String key = "Jecrisseizecarac"; // 128 bit key
+        private String maChaine = "Mogifalk"+"uneoudeuxcouilles";
 
-                public Key keyGen () {
-                    return new SecretKeySpec(key.getBytes(), "AES");
+        public StrongAES() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        }
 
-                }
+        public Key keyGen() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+            return new SecretKeySpec(this.hash(), "AES");
 
-                public byte[] encrypt(Key aesKey,String text) {
+        }
 
-                    Cipher cipher = null;
-                    try {
-                        cipher = Cipher.getInstance("AES");
-                    } catch (NoSuchAlgorithmException e1) {
-                        e1.printStackTrace();
-                    } catch (NoSuchPaddingException e1) {
-                        e1.printStackTrace();
-                    }
+        public byte[] encrypt(Key aesKey, String text) {
 
-                    try {
-                        cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-                    } catch (InvalidKeyException e1) {
-                        e1.printStackTrace();
-                    }
+            Cipher cipher = null;
+            try {
+                cipher = Cipher.getInstance("AES");
+            } catch (NoSuchAlgorithmException e1) {
+                e1.printStackTrace();
+            } catch (NoSuchPaddingException e1) {
+                e1.printStackTrace();
+            }
 
-                    byte[] encrypted = new byte[0];
+            try {
+                cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+            } catch (InvalidKeyException e1) {
+                e1.printStackTrace();
+            }
 
-                    try {
-                        encrypted = cipher.doFinal(text.getBytes());
-                    } catch (IllegalBlockSizeException e1) {
-                        e1.printStackTrace();
-                    } catch (BadPaddingException e1) {
-                        e1.printStackTrace();
-                    }
+            byte[] encrypted = new byte[0];
 
-                    System.err.println(new String(encrypted));
-                    return encrypted;
-                }
+            try {
+                encrypted = cipher.doFinal(text.getBytes());
+            } catch (IllegalBlockSizeException e1) {
+                e1.printStackTrace();
+            } catch (BadPaddingException e1) {
+                e1.printStackTrace();
+            }
+
+            System.err.println(new String(encrypted));
+            return encrypted;
+        }
 
 
-                public String decrypt(Key aesKey, byte[] encrypted) {
-                    Cipher cipher = null;
+        public String decrypt(Key aesKey, byte[] encrypted) {
+            Cipher cipher = null;
 
-                    try {
-                        cipher = Cipher.getInstance("AES");
-                    } catch (NoSuchAlgorithmException e1) {
-                        e1.printStackTrace();
-                    } catch (NoSuchPaddingException e1) {
-                        e1.printStackTrace();
-                    }
+            try {
+                cipher = Cipher.getInstance("AES");
+            } catch (NoSuchAlgorithmException e1) {
+                e1.printStackTrace();
+            } catch (NoSuchPaddingException e1) {
+                e1.printStackTrace();
+            }
 
-                    try {
-                        cipher.init(Cipher.DECRYPT_MODE, aesKey);
-                    } catch (InvalidKeyException e1) {
-                        e1.printStackTrace();
-                    }
-                    String decrypted = null;
-                    try {
-                        decrypted = new String(cipher.doFinal(encrypted));
-                    } catch (IllegalBlockSizeException e) {
-                        e.printStackTrace();
-                    } catch (BadPaddingException e) {
-                        e.printStackTrace();
-                    }
-                    System.err.println(decrypted);
-                    return decrypted ;
-                }
+            try {
+                cipher.init(Cipher.DECRYPT_MODE, aesKey);
+            } catch (InvalidKeyException e1) {
+                e1.printStackTrace();
+            }
+            String decrypted = null;
+            try {
+                decrypted = new String(cipher.doFinal(encrypted));
+            } catch (IllegalBlockSizeException e) {
+                e.printStackTrace();
+            } catch (BadPaddingException e) {
+                e.printStackTrace();
+            }
+            System.err.println(decrypted);
+            return decrypted;
+        }
 
-                //byte[] byteChaine = maChaine.getBytes(UTF-8);
-                //MessageDigest md = MessageDigest.getInstance(MD5);
-                //byte[] hash = md.digest(byteChaine);
-}
+        public byte[] hash() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+
+            byte[] byteChaine = maChaine.getBytes("UTF-8");
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            return md.digest(byteChaine);
+
+        }
+    }
--- src/com/adribast/clavarnak/StrongAESTest.java
+++ src/com/adribast/clavarnak/StrongAESTest.java
@@ -2,7 +2,10 @@ package com.adribast.clavarnak;
 
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -11,26 +14,33 @@ public class StrongAESTest {
 
     static byte[] texte ;
 
+    public StrongAESTest() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+    }
+
     @Test
-    public void keyGen() {
+    public void keyGen() throws UnsupportedEncodingException, NoSuchAlgorithmException {
         System.out.println(test.keyGen().toString());
     }
 
     @Test
-    public void encrypt() {
+    public void encrypt() throws UnsupportedEncodingException, NoSuchAlgorithmException {
         Key k = test.keyGen();
-        String t = "j espere que ca marche et que ca crypte bien";
+        String t = "Mogiflak";
         texte = (test.encrypt(k,t));
     }
 
     @Test
-    public void decrypt() {
+    public void decrypt() throws UnsupportedEncodingException, NoSuchAlgorithmException {
         Key k = test.keyGen();
 
-        System.out.println(texte);
+        System.out.println(Arrays.toString(texte));
         //String t = "j espere que ca marche et que ca crypte bien";
         //byte[] crypted = test.encrypt(k,t);
         System.out.println(test.decrypt(k,texte));
     }
 
+    @Test
+    public void hash() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        System.out.println(Arrays.toString(test.hash()));
+    }
 }
