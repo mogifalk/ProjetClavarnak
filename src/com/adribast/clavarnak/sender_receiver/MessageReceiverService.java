d0b14590c385943160ba5044acea0f1808a9f1e2
adri <adrien.gonzalez@gmail.com>
2017-12-16 17:17:59 +0100

    correcting conception

--- src/com/adribast/clavarnak/sender_receiver/MessageReceiverService.java
+++ src/com/adribast/clavarnak/sender_receiver/MessageReceiverService.java
@@ -4,12 +4,7 @@ import java.io.IOException;
 
 public interface MessageReceiverService extends Runnable {
 
-    /**
-     * Listen to incoming messages on given port, and notifies the listener of new messages
-     *
-     * @param port                    The port to listen on
-     * @param incomingMessageListener The listener to notify on new incoming messages
-     */
+
     void listen() throws Exception;
     void endConnection() throws IOException;
 }
