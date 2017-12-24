f22ce4d9c50492108fa09159631f0d3bbe06bda0
adri <adrien.gonzalez@gmail.com>
2017-12-24 01:53:08 +0100

    First free commit

--- src/com/adribast/clavarnak/sender_receiver/IncomingMessageListener.java
+++
@@ -1,8 +0,0 @@
-package com.adribast.clavarnak.sender_receiver;
-
-
-import java.io.IOException;
-
-public interface IncomingMessageListener {
-    void onNewIncomingMessage(String message) throws IOException, InterruptedException;
-}
--- src/com/adribast/clavarnak/sender_receiver/MessageSenderService.java
+++
@@ -1,9 +0,0 @@
-package com.adribast.clavarnak.sender_receiver;
-
-import java.io.IOException;
-
-public interface MessageSenderService {
-
-    void sendMessageOn(String message) throws Exception;
-    void endConnection() throws IOException;
-}
